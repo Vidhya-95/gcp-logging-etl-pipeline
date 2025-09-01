1d59d811cb140aafe7362406d9ea32dee6d2283f
Vidhya-95 <shinde.vidhya06@gmail.com>
2025-09-01 19:34:49 +0530

    Update outputs.tf
    Updated Outputs.tf file

--- terraform_scripts/outputs.tf
+++ terraform_scripts/outputs.tf
@@ -5,3 +5,19 @@ output "topic_name" {
 output "logging_sink_name" {
   value = google_logging_project_sink.tf_topic_sink.name
 }
+
+output "bucket_name" {
+  value = google_storage_bucket.tf_bucket.name
+}
+
+output "object_name" {
+  value = google_storage_bucket_object.tf_object.name
+}
+
+output "function_name" {
+  value = google_cloudfunctions2_function.tf_process_audit_logs_func.name
+}
+
+output "function_event_trigger_topic" {
+  value = google_cloudfunctions2_function.tf_process_audit_logs_func.event_trigger[0].pubsub_topic
+}
