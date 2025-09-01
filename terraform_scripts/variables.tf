a7be0034dd70658eefb4c73e7c21387b29cfb6ca
Vidhya-95 <shinde.vidhya06@gmail.com>
2025-09-01 19:42:16 +0530

    Updated variables.tf

--- terraform_scripts/variables.tf
+++ terraform_scripts/variables.tf
@@ -17,3 +17,27 @@ variable "logging_sink_name" {
   description = "This a name for logging sink"
 }
 
+variable "bucket_name" {
+  default = "tf_gcf-source"
+  description = "Bucket name to store the source code"
+}
+
+variable "bucket_location" {
+  default = "US"
+  description = "Value for the bucket location"
+}
+
+variable "object_name" {
+  default = "tf_process_audit_logs_function-source.zip"
+  description = "Value for the object name"
+}
+
+variable "function_name" {
+  default = "tf_process_audit_logs"
+  description = "Name of the function to process audit logs"
+}
+
+variable "function_location" {
+  default = "us-central1"
+  description = "Location of the function"
+}
