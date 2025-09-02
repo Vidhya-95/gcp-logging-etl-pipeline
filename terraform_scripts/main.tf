e6e45af3d4578c9550d4f26e1d3b058d2bf47644
Vidhya-95 <shinde.vidhya06@gmail.com>
2025-09-02 13:05:35 +0530

    Update main.tf

--- terraform_scripts/main.tf
+++ terraform_scripts/main.tf
@@ -13,3 +13,40 @@ AND operation.last="true"
 EOT
   unique_writer_identity = true
 }
+
+resource "google_storage_bucket" "tf_bucket" {
+  name                        = "${var.project_id}-${var.bucket_name}"
+  location                    = var.bucket_location
+  uniform_bucket_level_access = true
+}
+
+resource "google_storage_bucket_object" "tf_object" {
+  name   = var.object_name
+  bucket = google_storage_bucket.tf_bucket.name
+  source = "<PATH_TO_ZIPPED_SOURCE_CODE>" # Add path to the zipped function source code e.g.: "/gcp-error-etl-pipeline/process_audit_logs_function-source.zip"
+}
+
+resource "google_cloudfunctions2_function" "tf_process_audit_logs_func" {
+  name        = var.function_name
+  location    = var.function_location
+  description = "Function to process audit logs"
+
+  build_config {
+    runtime     = "python313"
+    entry_point = "main" # Set the entry point 
+
+    source {
+      storage_source {
+        bucket = google_storage_bucket.tf_bucket.name
+        object = google_storage_bucket_object.tf_object.name
+      }
+    }
+  }
+
+  event_trigger {
+    trigger_region = var.event_trigger_region
+    event_type     = "google.cloud.pubsub.topic.v1.messagePublished"
+    pubsub_topic   = google_pubsub_topic.tf_topic.id
+    retry_policy   = "RETRY_POLICY_RETRY"
+  }
+}
